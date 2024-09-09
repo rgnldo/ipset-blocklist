b4ae357453864620d7b0a842c4b001f6d0618eb8
rgnldo <58551674+rgnldo@users.noreply.github.com>
2024-09-09 20:54:48 -0300

    Update update-blocklist.sh

--- update-blocklist.sh
+++ update-blocklist.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 #
-# usage: update-blocklist.sh <configuration file>
+# usage update-blocklist.sh <configuration file>
 
-function exists() {
-  command -v "$1" >/dev/null 2>&1
-}
+#
+function exists() { command -v "$1" >/dev/null 2>&1 ; }
 
 if [[ -z "$1" ]]; then
   echo "Error: please specify a configuration file, e.g. $0 /opt/ipset-blocklist/ipset-blocklist.conf"
   exit 1
 fi
 
+# shellcheck source=ipset-blocklist.conf
 if ! source "$1"; then
   echo "Error: can't load configuration file $1"
   exit 1
 fi
 
-if ! exists curl || ! exists egrep || ! exists grep || ! exists ipset || ! exists iptables || ! exists sed || ! exists sort || ! exists wc ; then
+if ! exists curl && exists egrep && exists grep && exists ipset && exists iptables && exists sed && exists sort && exists wc ; then
   echo >&2 "Error: searching PATH fails to find executables among: curl egrep grep ipset iptables sed sort wc"
   exit 1
 fi
@@ -27,57 +27,55 @@ if exists iprange && [[ ${OPTIMIZE_CIDR:-yes} != no ]]; then
 fi
 
 if [[ ! -d $(dirname "$IP_BLOCKLIST") || ! -d $(dirname "$IP_BLOCKLIST_RESTORE") ]]; then
-  echo >&2 "Error: missing directory(s): $(dirname "$IP_BLOCKLIST" "$IP_BLOCKLIST_RESTORE" | sort -u)"
+  echo >&2 "Error: missing directory(s): $(dirname "$IP_BLOCKLIST" "$IP_BLOCKLIST_RESTORE"|sort -u)"
   exit 1
 fi
 
-for i in {1..5}; do
-  IPSET_NAME="${IPSET_BLOCKLIST_NAME}_${i}"
-  if ! ipset list -n | grep -q "^$IPSET_NAME$"; then
-    if [[ ${FORCE:-no} != yes ]]; then
-      echo >&2 "Error: ipset '$IPSET_NAME' does not exist yet, create it using:"
-      echo >&2 "# ipset create $IPSET_NAME hash:net family inet hashsize ${HASHSIZE:-32768} maxelem ${MAXELEM:-131072}"
-      exit 1
-    fi
-    if ! ipset create "$IPSET_NAME" hash:net family inet hashsize "${HASHSIZE:-32768}" maxelem "${MAXELEM:-131072}"; then
-      echo >&2 "Error: while creating the initial ipset '$IPSET_NAME'"
-      exit 1
-    fi
-  fi
-done
-
-if ! iptables -nvL INPUT | grep -q "match-set ${IPSET_BLOCKLIST_NAME}_1"; then
+# create the ipset if needed (or abort if does not exists and FORCE=no)
+if ! ipset list -n|command grep -q "$IPSET_BLOCKLIST_NAME"; then
   if [[ ${FORCE:-no} != yes ]]; then
-    echo >&2 "Error: iptables rule for ipset '${IPSET_BLOCKLIST_NAME}_1' is missing."
-    echo >&2 "Add it manually using:"
-    echo >&2 "# iptables -I INPUT ${IPTABLES_IPSET_RULE_NUMBER:-1} -m set --match-set ${IPSET_BLOCKLIST_NAME}_1 src -j DROP"
+    echo >&2 "Error: ipset does not exist yet, add it using:"
+    echo >&2 "# ipset create $IPSET_BLOCKLIST_NAME -exist hash:net family inet hashsize ${HASHSIZE:-16384} maxelem ${MAXELEM:-65536}"
     exit 1
   fi
-  if ! iptables -I INPUT "${IPTABLES_IPSET_RULE_NUMBER:-1}" -m set --match-set "${IPSET_BLOCKLIST_NAME}_1" src -j DROP; then
-    echo >&2 "Error: Failed to add iptables rule for ipset '${IPSET_BLOCKLIST_NAME}_1'."
+  if ! ipset create "$IPSET_BLOCKLIST_NAME" -exist hash:net family inet hashsize "${HASHSIZE:-16384}" maxelem "${MAXELEM:-65536}"; then
+    echo >&2 "Error: while creating the initial ipset"
+    exit 1
+  fi
+fi
+
+# create the iptables binding if needed (or abort if does not exists and FORCE=no)
+if ! iptables -nvL INPUT|command grep -q "match-set $IPSET_BLOCKLIST_NAME"; then
+  # we may also have assumed that INPUT rule n°1 is about packets statistics (traffic monitoring)
+  if [[ ${FORCE:-no} != yes ]]; then
+    echo >&2 "Error: iptables does not have the needed ipset INPUT rule, add it using:"
+    echo >&2 "# iptables -I INPUT ${IPTABLES_IPSET_RULE_NUMBER:-1} -m set --match-set $IPSET_BLOCKLIST_NAME src -j DROP"
+    exit 1
+  fi
+  if ! iptables -I INPUT "${IPTABLES_IPSET_RULE_NUMBER:-1}" -m set --match-set "$IPSET_BLOCKLIST_NAME" src -j DROP; then
+    echo >&2 "Error: while adding the --match-set ipset rule to iptables"
     exit 1
   fi
 fi
 
 IP_BLOCKLIST_TMP=$(mktemp)
-for url in "${BLOCKLISTS[@]}"; do
+for i in "${BLOCKLISTS[@]}"
+do
   IP_TMP=$(mktemp)
-  HTTP_RC=$(curl -L -A "blocklist-update/script/github" --connect-timeout 10 --max-time 10 -o "$IP_TMP" -s -w "%{http_code}" "$url")
-  if [[ $HTTP_RC -eq 200 || $HTTP_RC -eq 302 || $HTTP_RC -eq 0 ]]; then
-    grep -Po '^(?:\d{1,3}\.){3}\d{1,3}(?:/\d{1,2})?' "$IP_TMP" | sed -r 's/^0*([0-9]+)\.0*([0-9]+)\.0*([0-9]+)\.0*([0-9]+)$/\1.\2.\3.\4/' >> "$IP_BLOCKLIST_TMP"
+  (( HTTP_RC=$(curl -L -A "blocklist-update/script/github" --connect-timeout 10 --max-time 10 -o "$IP_TMP" -s -w "%{http_code}" "$i") ))
+  if (( HTTP_RC == 200 || HTTP_RC == 302 || HTTP_RC == 0 )); then # "0" because file:/// returns 000
+    command grep -Po '^(?:\d{1,3}\.){3}\d{1,3}(?:/\d{1,2})?' "$IP_TMP" | sed -r 's/^0*([0-9]+)\.0*([0-9]+)\.0*([0-9]+)\.0*([0-9]+)$/\1.\2.\3.\4/' >> "$IP_BLOCKLIST_TMP"
     [[ ${VERBOSE:-yes} == yes ]] && echo -n "."
-  elif [[ $HTTP_RC -eq 503 ]]; then
-    echo -e "\\nUnavailable (${HTTP_RC}): $url"
-  elif [[ $HTTP_RC -eq 429 ]]; then
-    echo -e "\\nRate limit exceeded (${HTTP_RC}): $url"
-    sleep 60  # Espera de 60 segundos antes de continuar
+  elif (( HTTP_RC == 503 )); then
+    echo -e "\\nUnavailable (${HTTP_RC}): $i"
   else
-    echo >&2 -e "\\nWarning: curl returned HTTP response code $HTTP_RC for URL $url"
+    echo >&2 -e "\\nWarning: curl returned HTTP response code $HTTP_RC for URL $i"
   fi
   rm -f "$IP_TMP"
 done
 
-sed -r -e '/^(0\.0\.0\.0|10\.|127\.|172\.1[6-9]\.|172\.2[0-9]\.|172\.3[0-1]\.|192\.168\.|22[4-9]\.|23[0-9]\.)/d' "$IP_BLOCKLIST_TMP" | sort -n | sort -mu > "$IP_BLOCKLIST"
+# sort -nu does not work as expected
+sed -r -e '/^(0\.0\.0\.0|10\.|127\.|172\.1[6-9]\.|172\.2[0-9]\.|172\.3[0-1]\.|192\.168\.|22[4-9]\.|23[0-9]\.)/d' "$IP_BLOCKLIST_TMP"|sort -n|sort -mu >| "$IP_BLOCKLIST"
 if [[ ${DO_OPTIMIZE_CIDR} == yes ]]; then
   if [[ ${VERBOSE:-no} == yes ]]; then
     echo -e "\\nAddresses before CIDR optimization: $(wc -l "$IP_BLOCKLIST" | cut -d' ' -f1)"
@@ -91,13 +89,14 @@ fi
 
 rm -f "$IP_BLOCKLIST_TMP"
 
-# Cria um arquivo de restauração para o ipset
+# family = inet for IPv4 only
 cat >| "$IP_BLOCKLIST_RESTORE" <<EOF
-create $IPSET_TMP_BLOCKLIST_NAME -exist hash:net family inet hashsize ${HASHSIZE:-32768} maxelem ${MAXELEM:-131072}
-create $IPSET_BLOCKLIST_NAME -exist hash:net family inet hashsize ${HASHSIZE:-32768} maxelem ${MAXELEM:-131072}
+create $IPSET_TMP_BLOCKLIST_NAME -exist hash:net family inet hashsize ${HASHSIZE:-16384} maxelem ${MAXELEM:-65536}
+create $IPSET_BLOCKLIST_NAME -exist hash:net family inet hashsize ${HASHSIZE:-16384} maxelem ${MAXELEM:-65536}
 EOF
 
-# Adiciona as IPs ao ipset temporário
+# can be IPv4 including netmask notation
+# IPv6 ? -e "s/^([0-9a-f:./]+).*/add $IPSET_TMP_BLOCKLIST_NAME \1/p" \ IPv6
 sed -rn -e '/^#|^$/d' \
   -e "s/^([0-9./]+).*/add $IPSET_TMP_BLOCKLIST_NAME \\1/p" "$IP_BLOCKLIST" >> "$IP_BLOCKLIST_RESTORE"
 
@@ -106,17 +105,9 @@ swap $IPSET_BLOCKLIST_NAME $IPSET_TMP_BLOCKLIST_NAME
 destroy $IPSET_TMP_BLOCKLIST_NAME
 EOF
 
-# Restaura os ipsets
-if ! ipset -file "$IP_BLOCKLIST_RESTORE" restore; then
-  echo >&2 "Error: Failed to restore ipset from file."
-  exit 1
-fi
+ipset -file  "$IP_BLOCKLIST_RESTORE" restore
 
 if [[ ${VERBOSE:-no} == yes ]]; then
   echo
   echo "Blacklisted addresses found: $(wc -l "$IP_BLOCKLIST" | cut -d' ' -f1)"
 fi
-
-# Limpeza final
-rm -f "$IP_BLOCKLIST_RESTORE"
-
