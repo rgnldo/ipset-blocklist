45a5f8f744d9bdcfe005ad47d1c0430ed80927b0
rgnldo <58551674+rgnldo@users.noreply.github.com>
2024-11-18 22:09:35 -0300

    Update instal_bloqIP.sh

--- instal_bloqIP.sh
+++ instal_bloqIP.sh
@@ -28,75 +28,28 @@ install_ipset_blocklist() {
 
     wget -O /opt/ipset-blocklist/ipset-blocklist.conf https://raw.githubusercontent.com/rgnldo/ipset-blocklist/master/ipset-blocklist.conf
 
-    # Gerar a lista em /opt/ipset-blocklist/ip-blocklist.restore
+    # Gerar a lista inicial
     /usr/local/sbin/update-blocklist.sh /opt/ipset-blocklist/ipset-blocklist.conf
-    
 
-# Criar o arquivo do serviço systemd
-cat <<EOF > /etc/systemd/system/ipset-blocklist.service
-[Unit]
-Description=IPSet Blocklist Service (executa uma única vez no boot)
-After=network-online.target
-
-[Service]
-Type=oneshot
-ExecStartPre=/usr/sbin/ipset flush blocklist
-ExecStart=/usr/sbin/ipset restore < /opt/ipset-blocklist/ip-blocklist.restore
-ExecStartPost=/usr/sbin/iptables -I INPUT -m set --match-set blocklist src -j DROP
-RemainAfterExit=no
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-    # Criar o arquivo do temporizador systemd
-   cat <<EOF > /etc/systemd/system/ipset-blocklist.timer
-[Unit]
-Description=Agendador de 6 em 6 horas para atualização do IPSet Blocklist
-
-[Timer]
-OnBootSec=5min
-OnUnitActiveSec=6h
-AccuracySec=1s
-Persistent=true
-
-[Service]
-ExecStart=/usr/local/sbin/update-blocklist.sh /opt/ipset-blocklist/ipset-blocklist.conf
-
-[Install]
-WantedBy=timers.target
-EOF
-
-
-    # Recarregar o systemd
-    systemctl daemon-reload
-
-    # Habilitar o temporizador e o serviço para iniciar após o boot
-    systemctl enable ipset-blocklist.timer
-    systemctl enable ipset-blocklist.service
-    systemctl start ipset-blocklist.timer
-    systemctl start ipset-blocklist.service
+    # Adicionar cron jobs
+    (crontab -l 2>/dev/null; echo "@reboot ipset restore < /opt/ipset-blocklist/ip-blocklist.restore && iptables -I INPUT -m set --match-set blocklist src -j DROP") | crontab -
+    (crontab -l 2>/dev/null; echo "0 */12 * * * /usr/local/sbin/update-blocklist.sh /opt/ipset-blocklist/ipset-blocklist.conf") | crontab -
 
     echo "Instalação concluída com sucesso."
 }
 
 # Função para desinstalar o IPSet Blocklist
 uninstall_ipset_blocklist() {
-    # Parar o serviço e temporizador systemd
-    systemctl stop ipset-blocklist.service
-    systemctl disable ipset-blocklist.service
-    systemctl stop ipset-blocklist.timer
-    systemctl disable ipset-blocklist.timer
-
     # Remover regras do iptables e ipset
-    iptables -D INPUT -m set --match-set blocklist  src -j DROP 2>/dev/null
+    iptables -D INPUT -m set --match-set blocklist src -j DROP 2>/dev/null
     ipset destroy blocklist
 
     # Remover arquivos e diretórios
     rm -rf /usr/local/sbin/update-blocklist.sh
     rm -rf /opt/ipset-blocklist
-    rm -f /etc/systemd/system/ipset-blocklist.service
-    rm -f /etc/systemd/system/ipset-blocklist.timer
+
+    # Remover entradas do cron
+    crontab -l 2>/dev/null | grep -v 'update-blocklist.sh' | grep -v 'ipset restore' | crontab -
 
     echo "Desinstalação concluída com sucesso."
 }
